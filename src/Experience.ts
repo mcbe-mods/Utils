79877b2a0b8a76b6bea796ce0a7a3ad1ad97b8b5
Lete114 <me@imlete.cn>
2023-07-27 14:35:41 +0800

    feat: add Calculating player experience

---
+++ src/Experience.ts
@@ -0,0 +1,106 @@
+/**
+ * Calculating player experience
+ */
+export default class Experience {
+  #level = 0
+  #xp = 0
+  #nextLevelRequiredXP = this.#calculateRequiredExp(this.#level)
+
+  /**
+   * detail: https://minecraft.fandom.com/wiki/Experience#Leveling_up
+   * @param {number} level current level
+   * @returns {number} level up required experience
+   */
+  #calculateRequiredExp(level: number) {
+    if (level < 16) return 2 * level + 7
+    if (level < 31) return 5 * level - 38
+
+    return 9 * level - 158
+  }
+
+  /**
+   * Get all experience from level 0 to current level
+   * @returns {number} total experience
+   */
+  getTotalXP() {
+    let totalXp = this.#xp
+    let level = this.#level
+    while (level > 0) {
+      level--
+      totalXp += this.#calculateRequiredExp(level)
+    }
+    return totalXp
+  }
+
+  /**
+   * @returns {number} Next level required experience
+   */
+  getNextLevelRequiredXP() {
+    return this.#nextLevelRequiredXP
+  }
+
+  /**
+   * Add experience
+   * @param {number} points required add experience points
+   * @returns {this}
+   */
+  addXP(points: number) {
+    this.#xp += points
+    while (this.#xp >= this.#nextLevelRequiredXP) {
+      this.#level++
+      this.#xp -= this.#nextLevelRequiredXP
+      this.#nextLevelRequiredXP = this.#calculateRequiredExp(this.#level)
+    }
+    return this
+  }
+
+  /**
+   * Get the current level of experience
+   * Assuming that the player is currently level 1 and the next level is level 2, and that level 2 requires 9 experience points ({@link getNextLevelRequiredXP}), and at this point the player only has 3 experience points, this function will return 3 experience points
+   * @returns {number} current level of experience
+   */
+  getXP() {
+    return this.#xp
+  }
+
+  /**
+   * If it is set to 8 experience points, then {@link getXP} gets the experience value of 8 experience points
+   * @param points required set experience points
+   * @returns {this}
+   */
+  setXP(points: number) {
+    this.#xp = 0
+    this.addXP(points)
+    return this
+  }
+
+  /**
+   * Add level, assuming player level 1, after adding 2 levels it will become level 3
+   * @param {number} level required add level
+   * @returns {this}
+   */
+  addLevel(level: number) {
+    this.#level = Math.max(0, this.#level + level)
+    this.#nextLevelRequiredXP = this.#calculateRequiredExp(this.#level)
+    return this
+  }
+
+  /**
+   * Get current level
+   * @returns {number}
+   */
+  getLevel() {
+    return this.#level
+  }
+
+  /**
+   * If set to level 8, the level {@link getLevel} gets is level 8
+   * @param level required set level
+   * @returns {this}
+   */
+  setLevel(level: number) {
+    this.#level = 0
+    this.addLevel(level)
+    return this
+  }
+}
